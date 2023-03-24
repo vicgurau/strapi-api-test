a387247045a3e5ed98b3bead07ed185df826329f
vicgurau <gurauvictor@gmail.com>
2023-03-24 11:09:44 +0200

    customized get and put method on core requests

--- my-project/src/api/article/controllers/article.js
+++ my-project/src/api/article/controllers/article.js
@@ -14,6 +14,16 @@ module.exports = createCoreController('api::article.article', ({ strapi }) => ({
     } catch (err){
       ctx.body = err
     }
+  },
+  async find(ctx){
+    ctx.query = { ...ctx.query, populate: 'author'}
+    const { data, meta } = await super.find(ctx)
+    meta.date = Date.now()
+    return {data, meta}
+  },
+  async create(ctx) {
+    ctx.request.body.data = { ...ctx.request.body.data, author: ctx.state.user}
+    return super.create(ctx);
   }
 }));
 
